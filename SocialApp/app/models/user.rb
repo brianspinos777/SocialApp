f6683f2793ceb19fb00102850ab3fedf84d624cd
brianspinos777 <brianspinos777@hotmail.com>
2017-06-15 17:34:59 -0700

    add paperclip gem

--- SocialApp/app/models/user.rb
+++ SocialApp/app/models/user.rb
@@ -1,6 +1,43 @@
 class User < ApplicationRecord
+
+  #======= DEVISE
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  #======= PAPErCLIP
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  #
+  # How to check the mime type of a file:
+  # $ file -b --mime-type  foo.docx
+  #
+  # => application/vnd.openxmlformats-officedocument.wordprocessingml.document
+  #
+  # validates_attachment_content_type :avatar, content_type: [
+  #       "application/pdf",
+  #       "application/zip",
+  #       "application/x-zip",
+  #       "application/x-zip-compressed",
+  #       "application/octet-stream",
+  #       "application/vnd.ms-office",
+  #       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  #       "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  #       "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  #       "application/pdf",
+  #       "text/plain",
+  #       "image/jpg",
+  #       "image/png",
+  #       "image/jpeg",
+  #       /\Aimage\/.*\Z/
+  # ]
+
+
+
+
 end
---
+++ SocialApp/config/initializers/paperclip.rb
@@ -0,0 +1,10 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+#
+# If you are seeing the following error:
+#    “The bucket you are attempting to access must be addressed using the
+#     specified endpoint. Please send all future requests to this endpoint.”
+#     Try setting the specified endpoint with the s3_host_name config var.
+#
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
