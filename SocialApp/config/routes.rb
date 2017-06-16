8834612c703a388ec3b814632d5d618c0a6fc846
brianspinos777 <brianspinos777@hotmail.com>
2017-06-15 17:17:50 -0700

    added a landing page

--- SocialApp/config/routes.rb
+++ SocialApp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root to: "users#index"
+  root to: "home#index"
 
   # devise_scope :user do
   #   root to: "devise/sessions#new"
